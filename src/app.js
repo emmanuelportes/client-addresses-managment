af97dfc3318e0eedc4a70202723716ab3cb735da
Emmanuel Portes <mportesz.08@gmail.com>
2022-08-31 11:55:24 -0400

    added create feature

--- src/app.js
+++ src/app.js
@@ -17,12 +17,32 @@ app.use(express.urlencoded({
     extended:true
  }));
 
- app.get('/', (req, res) => {
-    res.send(`${process.env.PORT}`)
- })
+ app.get('/', async(request, response) => {
+	try {
+		const clients = await pool.query("SELECT clients.client_id, clients.name, clients.lastname, addresses.address FROM clients JOIN addresses ON clients.client_id = addresses.client_id;");
+    	response.status(200).json(clients.rows);
 
-app.post('/', (request, response) => {
-    response.send(request.body);
+	} catch(err){
+		console.log(err.stack);
+		response.json({"message":"an error just ocurred"})
+	}
+ });
+
+app.post('/', async(request, response) => {
+    try {
+        const { name, lastname, addresses } = request.body;
+        const result = await pool.query("INSERT INTO clients(name, lastname) VALUES($1, $2) RETURNING client_id;", [name, lastname]);
+
+        for (const address of addresses) { 
+            pool.query("INSERT INTO addresses(address, client_id) VALUES($1, $2);", [address, result.rows[0].client_id]);
+        };
+
+        response.status(200).json({'message':'client inserted successfully'})
+
+    } catch(err) {
+        console.log(err.stack);
+        response.status(500).json({"message":"an error just occured"})
+    }
 });
 
 app.listen(process.env.PORT, ()=>{
